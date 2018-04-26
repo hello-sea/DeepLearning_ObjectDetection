0e4d2d824851d35c2f3455d3ecb507342b910849
Sea <hello.sea@qq.com>
2018-04-26 15:30:00 +0800

    update

--- v0.3/use_myCNNs_model.py
+++ v0.3/use_myCNNs_model.py
@@ -4,38 +4,107 @@ import numpy as np
 import tensorflow as tf
 import pickle as pickle # python pkl 文件读写
 
-import myCNNs_model
-
-if __name__ == "__mian__":
-
-    saver = tf.train.Saver()  
-    
-    with tf.Session() as sess:  
-        saver.restore(sess, "./Model/cnn_model.ckpt") # 注意此处路径前添加"./"  
-        
-        # Evaluate the model and print results
-        eval_input_fn = tf.estimator.inputs.numpy_input_fn(
-            x={"x": eval_data},
-            y=eval_labels,
-            num_epochs=1,
-            shuffle=False) 
-        eval_results = cnn_classifier.evaluate(input_fn=eval_input_fn)
-        print(eval_results)
+# import myCNNs_model
 
 
+def cnn_model_fn(features, labels, mode):
+    """Model function for CNN."""
+    # Input Layer
+    # Reshape X to 4-D tensor: [batch_size, width, height, channels]
+    # MNIST images are 28x28 pixels, and have one color channel
+    input_layer = tf.reshape(features["x"], [-1, 28, 28, 3])
+
+    # Convolutional Layer #1
+    # Computes 32 features using a 5x5 filter with ReLU activation.
+    # Padding is added to preserve width and height.
+    # Input Tensor Shape: [batch_size, 28, 28, 1]
+    # Output Tensor Shape: [batch_size, 28, 28, 32]
+    conv1 = tf.layers.conv2d(
+        inputs=input_layer,
+        filters=32,
+        kernel_size=[5, 5],
+        padding="same",
+        activation=tf.nn.relu)
+
+    # Pooling Layer #1
+    # First max pooling layer with a 2x2 filter and stride of 2
+    # Input Tensor Shape: [batch_size, 28, 28, 32]
+    # Output Tensor Shape: [batch_size, 14, 14, 32]
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+
+    # Convolutional Layer #2
+    # Computes 64 features using a 5x5 filter.
+    # Padding is added to preserve width and height.
+    # Input Tensor Shape: [batch_size, 14, 14, 32]
+    # Output Tensor Shape: [batch_size, 14, 14, 64]
+    conv2 = tf.layers.conv2d(
+        inputs=pool1,
+        filters=64,
+        kernel_size=[5, 5],
+        padding="same",
+        activation=tf.nn.relu)
+
+    # Pooling Layer #2
+    # Second max pooling layer with a 2x2 filter and stride of 2
+    # Input Tensor Shape: [batch_size, 14, 14, 64]
+    # Output Tensor Shape: [batch_size, 7, 7, 64]
+    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+
+    # Flatten tensor into a batch of vectors
+    # Input Tensor Shape: [batch_size, 7, 7, 64]
+    # Output Tensor Shape: [batch_size, 7 * 7 * 64]
+    pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
+
+    # Dense Layer
+    # Densely connected layer with 1024 neurons
+    # Input Tensor Shape: [batch_size, 7 * 7 * 64]
+    # Output Tensor Shape: [batch_size, 1024]
+    dense = tf.layers.dense(
+        inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+
+    # Add dropout operation; 0.6 probability that element will be kept
+    dropout = tf.layers.dropout(
+        inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
+
+    # Logits layer
+    # Input Tensor Shape: [batch_size, 1024]
+    # Output Tensor Shape: [batch_size, 30]
+    logits = tf.layers.dense(inputs=dropout, units=30)
+
+    predictions = {
+        # Generate predictions (for PREDICT and EVAL mode)
+        "classes": tf.argmax(input=logits, axis=1),
+        # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
+        # `logging_hook`.
+        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+    }
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+    # Calculate Loss (for both TRAIN and EVAL modes)
+    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+
+    # Configure the Training Op (for TRAIN mode)
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+        train_op = optimizer.minimize(
+            loss=loss,
+            global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+    # Add evaluation metrics (for EVAL mode)
+    eval_metric_ops = {
+        "accuracy": tf.metrics.accuracy(
+            labels=labels, predictions=predictions["classes"])}
+    return tf.estimator.EstimatorSpec(
+        mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
+
+''' ***************************************************************** '''
 
 
 def main(unused_argv):
 
-    train_data = np.array(pickle.load(open('Model/train_data.plk', 'rb')) )
-    train_labels = np.array(pickle.load(open('Model/train_labels.plk', 'rb')) )
-
-    eval_data = np.array(pickle.load(open('Model/eval_data.plk', 'rb')) )
-    eval_labels = np.array(pickle.load(open('Model/eval_labels.plk', 'rb')) )
-
-    # with tf.Session() as sess:
-    #     train_data = tf.convert_to_tensor(train_data_np)
-    #     eval_data = tf.convert_to_tensor(eval_data_np)
 
     # Create the Estimator
     cnn_classifier = tf.estimator.Estimator(
@@ -48,41 +117,26 @@ def main(unused_argv):
     logging_hook = tf.train.LoggingTensorHook(
         tensors=tensors_to_log, every_n_iter=50)
 
-
-    # Train the model
-    train_input_fn = tf.estimator.inputs.numpy_input_fn(
-        x={"x": train_data},
-        y=train_labels,
-        batch_size=50,
-        num_epochs=None,
-        shuffle=True)
-    cnn_classifier.train(
-        input_fn=train_input_fn,
-        steps=200,     # steps=20000,
-        hooks=[logging_hook])
-
     # 保存模型
     cnn_classifier.export_savedmodel("Model/cnn_model.ckpt", train_input_fn)
 
-    '''
-    # 加载模型
-    saver = tf.train.Saver()  
     
+    # 加载模型
+    saver = tf.train.Saver()
     with tf.Session() as sess:  
         saver.restore(sess, "./Model/cnn_model.ckpt") # 注意此处路径前添加"./"  
-    '''
 
-    # Evaluate the model and print results
-    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
-        x={"x": eval_data},
-        y=eval_labels,
-        num_epochs=1,
-        shuffle=False) 
-    eval_results = cnn_classifier.evaluate(input_fn=eval_input_fn)
-    print(eval_results)
+        # Evaluate the model and print results
+        eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+            x={"x": eval_data},
+            y=eval_labels,
+            num_epochs=1,
+            shuffle=False) 
+        eval_results = cnn_classifier.evaluate(input_fn=eval_input_fn)
+        print(eval_results)
 
 
-if __name__ == "__main__":
-    
-    tf.app.run()
+if __name__ == "__mian__":
+    main()
+
 
